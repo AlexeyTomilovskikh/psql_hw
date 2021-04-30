59d2f8cf968211766d238fa5549a3278210a6072
Alexey Tomilovskikh <alexey.tomilovskikh@gmail.com>
2021-04-30 21:13:03 +0500

    hw_2

--- Table_Artist.sql
+++ Table_Artist.sql
@@ -1,28 +1,41 @@
-create table if not exists Site_features (
+create table if not exists artist_list(
 	id serial primary key,
-	Search_by_artist varchar(80) not null,
-	Search_by_genre varchar(80) not null);
-	
-create table if not exists Genre_list(
-	id serial primary key, 
-	Genre_id intrger references Site_features(id),
-	Genre_Name  varchar(80) not null,
-	Singers_in_this_genre varchar(255) not null);
-	
-create table if not exists Artist_list(
-	id serial primary key,
-	Artist_id integer references Site_features(id),
 	Name varchar(255) not null,
 	Alias varchar(80) not null);
 	
-create table if not exists List_Albums (
+create table if not exists artist_in_genre(
 	id serial primary key,
-	album_id integer references Artist_list(id),
-	AlbumName varchar(80) not null,
-	ReleaseDate varchar(80) not null);
+	artist_id integer not null references artist_list(id),
+	genre_id integer not null references Genre_List(id));
 	
-create table if not exists List_Track (
+create table if not exists Genre_list(
+	id serial primary key, 
+	Genre_Name  varchar(80) not null,
+	Singers_in_this_genre varchar(255) not null);
+							
+create table if not exists artist_album(
+	id  serial primary key,
+	artist_id integer not null references artist_list(id),
+	album_id integer not null references album_List(id));
+	
+create table if not exists album_List (
 	id serial primary key,
-	Track_id integer references List_Albums(id),
-	Track_Name varchar(80) not null,
-	Track_Time varchar(80) not null);
+	album_name varchar(80) not null,
+	release_date varchar(80) not null);
+	
+create table if not exists Track_List (
+	id serial primary key,
+	album_id integer references album_List(id)
+	track_name varchar(80) not null,
+	track_time varchar(80) not null);
+		
+create table if not exists collection_List(
+	id serial primary key,
+	collection_name varchar(60) not null,
+	release_date  not null);
+							
+create table if not exists collection_album_track(
+	id serial primary key,
+	collection_id integer not null references collection_List(id),
+	album_id integer not null references album_List(id),
+	track_id integer not null references Track_List(id));
